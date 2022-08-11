cd13784163323ecf6e58768be741826cea0586db
Daniel Conlon <danielcconlon@gmail.com>
2022-08-10 20:40:58 -0400

    replies are now connected

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const {
+    addComment,
+    removeComment,
+    addReply,
+    removeReply
+  } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router.route('/:pizzaId').post(addComment);
+
+//  /api/comments/:pizzaid/:commentId
+// handle addNewReply()
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// handle removeReply()
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 // /api/comments/<pizzaId>/<commentId>
 router.route('/:pizzaId/:commentId').delete(removeComment);
